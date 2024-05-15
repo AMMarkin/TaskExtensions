15affd7ce66f8f9153b29b8f3dd267a7af782f08
suicidalTendencies <ytrewqqwerty1024@yandex.ru>
2024-05-16 01:24:56 +0300

    Добавлена пара примеров использования

--- TaskExtension/TestClass.cs
+++ TaskExtension/TestClass.cs
@@ -1,10 +1,3 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
 namespace TaskExtension;
 internal class TestClass
 {
@@ -12,13 +5,25 @@ internal class TestClass
     {
         var task = Task.Delay(1000);
 
-        await task.Catch<Exception>(x => { });
+        await task.Catch<NullReferenceException>(x => { });
 
         var result = await GetStringAsync().Catch<NullReferenceException>(x => null!)
             .Catch(e => throw e);
 
-        var intresult = await Task.FromResult(1).Catch<NullReferenceException>(x => 1);
+        var intResult = await Task.FromResult(1).Catch<NullReferenceException>(x => 1);
+
+        var tResult = await Test<int>().Catch<NullReferenceException>(x => 1);
+
+        var withAnonimousTypes = Enumerable.Range(1, 100)
+            .Select(x => new { value = x, text = x.ToString() })
+            .Select(x => x.value);
     }
-    public Task<IEnumerable<string>> GetStringAsync()
-        => Task.FromResult(Enumerable.Empty<string>());
+
+    public Task<IEnumerable<string>> GetStringAsync() => null!;
+
+    public Task<TResult> Test<TResult>() =>null!;
+
+    public Task<Result<TResult>> Test2<TResult>() => null!;
+
+    public class Result<T>;
 }
